a5a3b6d28ef9b09b9cd1f6a994ad6c5d6f707255
Eduard <nikolaef.post@gmail.com>
2021-05-13 10:45:17 +0300

    test

---
+++ HW1/src/index.js
@@ -0,0 +1,22 @@
+let chalk = require('chalk');
+function anagram(str1, str2) {
+
+  console.log(strPreparation(str1), strPreparation(str2));
+  if (strPreparation(str1)===strPreparation(str2)) return true;
+  else return false;
+
+}
+
+function strPreparation(str) {
+  let format_str = '';
+  for (let i = 0; i < str.length; i++) {
+    let letter = str.charCodeAt(i);
+    if(letter > 64 && letter < 91 || letter > 96 && letter < 123 ||
+    letter > 1039 && letter <1104 || letter == 1025 || letter == 1105 ){
+      format_str +=str[i].toLowerCase();
+    }
+  }
+  return  format_str.split('').sort().join('');
+}
+
+console.log(chalk.green(anagram('pklji  ,fpp', 'fkljippp')));
