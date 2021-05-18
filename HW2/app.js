0c129d31089f58c5e72acbcdf194da0452b1bbb1
Eduard <nikolaef.post@gmail.com>
2021-05-18 13:25:46 +0300

    hw2 comleted

--- HW2/app.js
+++ HW2/app.js
@@ -1,18 +1,51 @@
+    const validator = require('validator');
     const express = require('express');
     const path = require('path');
+    const {v4} = require('uuid')
     const app =  express();
 
-    const CONTACTS = [
-        {id: 1, name: 'Ed', value: '0939824980', marked: false}
+    let CONTACTS = [
+        {id: v4(), name: 'Ed', value: '0939824980', marked: false}
     ]
 
+    app.use(express.json())
 
 
+// GET
+    app.get('/api/v1/users', (req, res)=>{
+        setTimeout(()=>{
+            res.status(200).json(CONTACTS)
+        },1000)
+    })
 
+// POST
+    app.post('/api/v1/users', (req, res)=>{
 
+        if (req.body.name.length<2)
+            return res.status(206).json({massage: 'the user must have a name greater than two characters'})
+        if (!validator.isMobilePhone(req.body.value, 'uk-UA'))
+            return res.status(206).json({massage: 'enter a number in the format "0-- -------"'})
+       const contact = {...req.body, id: v4(), marked: false}
+        CONTACTS.push(contact)
+        res.status(201).json(contact)
+    })
 
+// DELETE
+    app.delete('/api/v1/users:id', (req, res)=>{
+        CONTACTS = CONTACTS.filter(c=> c.id !== req.params.id)
+        res.status(200).json({massage: 'contact has been deleted'})
+    })
 
-
+// PUT
+    app.put('/api/v1/users:id', (req, res)=>{
+        if (req.body.name.length<2)
+            return res.status(206).json({massage: 'the user must have a name greater than two characters'})
+        if (!validator.isMobilePhone(req.body.value, 'uk-UA'))
+            return res.status(206).json({massage: 'enter a number in the format "0-- -------"'})
+       const idx = CONTACTS.findIndex(c=>c.id === req.params.id)
+        CONTACTS[idx] = req.body
+        res.json(CONTACTS[idx])
+    })
 
 
     app.use(express.static(path.resolve(__dirname, 'client')));
@@ -20,16 +53,5 @@
     app.get('*', (req, res)=>{
         res.send(path.resolve(__dirname, 'client', 'index.html'));
     })
-
-    // let users = [];
-    // app.post('/api/v1/users', (req, res) => {
-    //     let user = req.query.user;
-    //     users.push(user);
-    //     res.status(200).send('ok');
-    // });
-    //
-    // app.get('/api/v1/users', (req, res) =>{
-    //     res.status(200).send(users.toString());
-    // })
 
     app.listen(80, ()=> console.log('Server has been started on port 80...'));
--- HW2/client/frontend.js
+++ HW2/client/frontend.js
@@ -1,9 +1,24 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js'
 
+Vue.component('loader',{
+    template:`
+    <div>
+         <p class="inline-flex items-center px-4 py-2  text-base leading-6 font-medium text-white  transition ease-in-out duration-150 cursor-not-allowed" >
+            <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            please wait ...
+        </p>
+    </div>
+    `
+})
+
 new Vue({
     el: '#app',
     data(){
         return {
+            loading: false,
             form: {
                 name: '',
                 value: ''
@@ -17,36 +32,67 @@ new Vue({
         }
     },
     methods:{
-        createContact(){
-           const {...contact} = this.form
-            this.contacts.push({...contact, id: Date.now(), marked: false})
+        async createContact(){
+            const {...contact} = this.form
+
+            const newContact = await request('/api/v1/users', 'POST',  contact)
+            if (newContact.massage){
+                alert(newContact.massage)
+                return
+            }
+            this.contacts.push(newContact)
+            console.log(newContact)
 
             this.form.name = this.form.value = ''
         },
-        markContact(id){
-           const  contact = this.contacts.find(c=>c.id===id)
+        async markContact(id){
+            const {...change_contact} = this.form
+            const  contact = this.contacts.find(c=>c.id===id)
+            const updated = await request(`/api/v1/users${id}`,'PUT', {
+                ... contact,
+                name: change_contact.name,
+                value: change_contact.value,
+                marked: false
+            })
+            if (updated.massage){
+                alert(updated.massage)
+                return
+            }
             contact.marked = true
+            contact.name = updated.name
+            contact.value = updated.value
+            this.form.name = this.form.value = ''
         },
-        removeContact(id){
+        async removeContact(id){
+            await request(`/api/v1/users${id}`,'DELETE')
             this.contacts = this.contacts.filter( c => c.id!==id)
         }
+    },
+    async mounted(){
+        this.loading = true
+        this.contacts  = await  request('/api/v1/users')
+        this.loading = false
+
     }
 })
 
 async function request(url, method = 'GET', data = null) {
     try {
-        const header = {}
+        const headers = {}
         let body
-        if(data){
-            header['Content-Type'] = 'application/json '
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
             body = JSON.stringify(data)
         }
-        const response = fetch(url, {
+
+        const response = await fetch(url, {
             method,
-            header
+            headers,
+            body
         })
-        return  await response.json()
+        return await response.json()
     } catch (e) {
-        console.warn('Error', e.message)
+        console.warn('Error:', e.message)
     }
 }
