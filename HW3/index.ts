7e58269526e1c3e4b6d4d1d714e93d3e2e546344
Eduard <nikolaef.post@gmail.com>
2021-05-22 18:29:44 +0300

    ///

--- HW3/index.ts
+++ HW3/index.ts
@@ -1,5 +1,7 @@
+import {Chart, Point} from 'chart.js';
 
-let data: (string| number)[][] = [
+
+let data = [
     ['January', 10000],
     ['February', 3000],
     ['March', 4000],
@@ -17,21 +19,40 @@ class Schedule {
     constructor(data: (string| number)[][]) {
         this.data_ = data;
         this.base = getMaxOfArray(this.data_.map(el=>el[1]));
-        let container: HTMLElement = document.getElementById('field_rend')
-        let coordinate: HTMLElement = document.getElementById('x_coord_rend')
+        let container: HTMLElement|null = document.getElementById('field_rend')
+        let coordinateX: HTMLElement|null = document.getElementById('x_coord_rend')
+
+        let coordinateY: HTMLElement|null = document.getElementById('y_coord_rend')
+
+        let span1: HTMLSpanElement = document.createElement("span");
+        span1.innerText = `${this.base}`;
+        let span2: HTMLSpanElement = document.createElement("span");
+        span2.innerText = `${this.base/2}`;
+        coordinateY!.append(span1, span2, document.createElement("span"));
 
         this.data_.forEach(el=>{
             let element: HTMLDivElement = document.createElement("div");
-            element .classList.add('w-20', 'bg-red-600');
-            element .style.height = `${Math.round(el[1]/this.base*100)}%`;
-            container.append(element );
+            element.classList.add('w-20', 'bg-red-600');
+            element.style.height = `${Math.round(el[1]/this.base*100)}%`;
+            container!.append(element );
 
             let coord_x: HTMLSpanElement = document.createElement("span");
+            coord_x.classList.add('inline-block', 'w-20', 'text-center');
             coord_x.innerText = el[0];
-            coordinate.append(coord_x );
+            coordinateX!.append(coord_x );
 
         })
     }
 }
+
+class PieChart {
+    data_: Array<object>;
+    constructor(data: (string| number)[][]) {
+        this.data_ = data;
+
+    }
+}
+
+
 
 let schedule: Schedule = new Schedule(data);
