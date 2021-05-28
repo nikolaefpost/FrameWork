d368aa20b44444065af3a632efea390f1b4a8aab
38093 <nikolaef.post@gmail.com>
2021-05-28 20:17:41 +0300

    load html

--- HW4/index.js
+++ HW4/index.js
@@ -1,14 +1,53 @@
+//https://www.youtube.com/watch?v=3aGSqasVPsI
+//npm i nodemon -D
+//запуск npm run dev
+
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const server = http.createServer((req, res) =>{
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
+    let filePath = path.join(__dirname, 'Public', req.url === '/'? 'main.html' : req.url);
+    console.log(filePath)
+    const ext = path.extname(filePath);
+    let contentType = 'text/html'
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        default:
+            contentType = 'text/html'
+    }
+    if(!ext){
+        filePath+='.html';
+    }
+    fs.readFile(filePath , (err, content)=>{
+        if (err){
+            fs.readFile(path.join(__dirname, 'Public', 'error.html'), (err, data) =>{
+                if(err){
+                    res.writeHead(500);
+                    res.end('Error')
+                }else {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200, {
+                'Content-Type': contentType
+            });
+            res.end(content)
+        }
+    })
 
-server.listen(port, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
+const PORT = process.env.PORT || 3000
+server.listen(PORT, ()=>{
+    console.log(`Server has been started on ${PORT}...`)
+})
