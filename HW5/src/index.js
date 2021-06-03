5ad18a653b5016bbdf95e3d2583f34b00a318c68
38093 <nikolaef.post@gmail.com>
2021-06-03 20:29:55 +0300

    data generation faker use schema.graphql

--- HW5/src/index.js
+++ HW5/src/index.js
@@ -2,51 +2,100 @@ import { GraphQLClient, gql } from 'graphql-request'
 import faker from 'faker'
 
 
-async function usersGenerate(userIds) {
+// async function usersGenerate(userIds) {
+//     const endpoint = 'http://localhost:8080/graphql'
+//
+//     const graphQLClient = new GraphQLClient(endpoint)
+//
+//     const mutation = gql`
+//         mutation addUser($users: [AddUserInput!]!) {
+//             addUser(input: $users) {
+//                 user {
+//                     id
+//                     username
+//                     avatar
+//                     status
+//                     active
+//                 }
+//             }
+//         }
+//     `
+//
+//     let users = [];
+//     for (let i = 0; i < 100; i++) {
+//         users.push(
+//             {
+//                 username: faker.internet.userName(),
+//                 avatar: faker.internet.avatar(),
+//                 status: faker.lorem.sentence(),
+//                 active: faker.datatype.boolean()
+//             }
+//         )
+//     }
+//     const variables = {
+//         users: users
+//     }
+//     console.log('start')
+//     const data = await graphQLClient.request(mutation, variables)
+//     userIds.push(...data.addUser.user.map(u => u.id))
+//
+//     console.log(JSON.stringify(data, undefined, 2))
+// }
+//
+//
+// async function generate() {
+//     let userIds = []
+//     await usersGenerate(userIds)
+//     console.log(userIds)
+// }
+
+async function productsGenerate(productIds) {
     const endpoint = 'http://localhost:8080/graphql'
 
     const graphQLClient = new GraphQLClient(endpoint)
 
     const mutation = gql`
-        mutation addUser($users: [AddUserInput!]!) {
-            addUser(input: $users) {
-                user {
+        mutation addProduct($products: [AddProductInput!]!) {
+            addProduct(input: $products) {
+                product {
                     id
-                    username
-                    avatar
-                    status
-                    active
+                    category
+                   
                 }
             }
         }
     `
 
-    let users = [];
-    for (let i = 0; i < 100; i++) {
-        users.push(
+    let products = [];
+    for (let i = 0; i < 5; i++) {
+        products.push(
             {
-                username: faker.internet.userName(),
-                avatar: faker.internet.avatar(),
-                status: faker.lorem.sentence(),
-                active: faker.datatype.boolean()
+                category: faker.commerce.department(),
+                subcategory: faker.commerce.productName(),
+                name: faker.commerce.product(),
+                price: faker.commerce.price(),
+                image_src: faker.image.technics(),
+                vendor_code: faker.datatype.number(),
+                quantity: faker.datatype.number(),
+                availability: faker.datatype.boolean()
             }
         )
     }
     const variables = {
-        users: users
+        products: products
     }
     console.log('start')
     const data = await graphQLClient.request(mutation, variables)
-    userIds.push(...data.addUser.user.map(u => u.id))
+    productIds.push(...data.addProduct.product.map(u => u.id))
 
     console.log(JSON.stringify(data, undefined, 2))
 }
 
 
 async function generate() {
-    let userIds = []
-    await usersGenerate(userIds)
-    console.log(userIds)
+    let productIds = []
+    await productsGenerate(productIds)
+    console.log(productIds)
 }
 
 generate();
